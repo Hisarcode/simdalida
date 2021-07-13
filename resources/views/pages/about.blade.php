b01d341ec3d5fa5127cea55c3bb3affd8066e58e
hisarcode <hisarcode@gmail.com>
2021-07-13 09:59:23 +0700

    add about view

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.landing')
+
+@section('content')
+    <!-- Blog Single -->
+    <section class="news-area archive blog-single section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="blog-single-main">
+                                <div class="main-image">
+                                    <h2 class="sidebar-title blog-title pb-4">Tentang Kami</h2>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5 offset-lg-1 col-md-6 col-12">
+                            <!-- About Video -->
+                            <div class="modern-img-feature">
+                                <img src="https://via.placeholder.com/800x725" alt="#">
+
+                            </div>
+                            <!--/End About Video  -->
+                        </div>
+                        <div class="col-lg-5 col-md-6 col-12">
+                            <div class="about-content section-title default text-left">
+
+                                <div class="section-bottom">
+                                    <div class="text">
+                                        <p>Lorem Ipsum Dolor Sit Amet, Consectetur AdipiscinE Tellus, Luctus Nec
+                                            Ullamcorper Mattis, Pulvinr Dapius Leo.Greater Great Set Seasons
+                                            Praesent auctor facilisis</p>
+                                        <p>Greater great set seasons was morning creepeth all made replen fisher
+                                            night to. She to fourth does. cattle also be days second sit given can
+                                            itself you good for better solution. morning creepeth all made replen
+                                            fisher night to. She</p>
+                                        <p>Lorem Ipsum Dolor Sit Amet, Consectetur AdipiscinE Tellus, Luctus Nec
+                                            Ullamcorper Mattis, Pulvinr Dapius Leo.Greater Great Set Seasons
+                                            Praesent auctor facilisis</p>
+                                        <p>Greater great set seasons was morning creepeth all made replen fisher
+                                            night to. She to fourth does. cattle also be days second sit given can
+                                            itself you good for better solution. morning creepeth all made replen
+                                            fisher night to. She</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--/ End Services -->    
+@endsection
