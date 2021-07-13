2183259ea5de16e5351fe17988ee7149619013d9
hisarcode <hisarcode@gmail.com>
2021-07-13 09:58:27 +0700

    add inforgrahic view

---
+++ resources/views/pages/infographic.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.landing')
+
+@section('content')    
+    <!-- Blog Single -->
+    <section class="news-area archive blog-single section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="blog-single-main">
+                                <div class="main-image">
+                                    <h2 class="sidebar-title blog-title pb-4">Inovasi-Inovasi Daerah</h2>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row mt-5">
+                        <div class="col-12">
+                            <div class="portfolio-main">
+                                <div id="portfolio-item" class="portfolio-item-active">
+                                    <div class="cbp-item business animation">
+                                        <!-- Single Portfolio -->
+                                        <div class="single-portfolio">
+                                            <div class="portfolio-head overlay">
+                                                <img src="https://via.placeholder.com/600x415" alt="#">
+                                                <a class="more" href="portfolio-single.html"><i
+                                                        class="fa fa-long-arrow-right"></i></a>
+                                            </div>
+                                            <div class="portfolio-content">
+                                                <h4><a href="portfolio-single.html">Creative Marketing</a></h4>
+                                                <p>Business, Aniamtion</p>
+                                            </div>
+                                        </div>
+                                        <!--/ End Single Portfolio -->
+                                    </div>
+                                    <div class="cbp-item seo consulting">
+                                        <!-- Single Portfolio -->
+                                        <div class="single-portfolio">
+                                            <div class="portfolio-head overlay">
+                                                <img src="https://via.placeholder.com/600x415" alt="#">
+                                                <a class="more" href="portfolio-single.html"><i
+                                                        class="fa fa-long-arrow-right"></i></a>
+                                            </div>
+                                            <div class="portfolio-content">
+                                                <h4><a href="portfolio-single.html">Creative Marketing</a></h4>
+                                                <p>Seo, Consulting</p>
+                                            </div>
+                                        </div>
+                                        <!--/ End Single Portfolio -->
+                                    </div>
+                                    <div class="cbp-item marketing seo">
+                                        <!-- Single Portfolio -->
+                                        <div class="single-portfolio">
+                                            <div class="portfolio-head overlay">
+                                                <img src="https://via.placeholder.com/600x415" alt="#">
+                                                <a class="more" href="portfolio-single.html"><i
+                                                        class="fa fa-long-arrow-right"></i></a>
+                                            </div>
+                                            <div class="portfolio-content">
+                                                <h4><a href="portfolio-single.html">Creative Marketing</a></h4>
+                                                <p>Marketing, SEO</p>
+                                            </div>
+                                        </div>
+                                        <!--/ End Single Portfolio -->
+                                    </div>
+                                    <div class="cbp-item animation branding">
+                                        <!-- Single Portfolio -->
+                                        <div class="single-portfolio">
+                                            <div class="portfolio-head overlay">
+                                                <img src="https://via.placeholder.com/600x415" alt="#">
+                                                <a class="more" href="portfolio-single.html"><i
+                                                        class="fa fa-long-arrow-right"></i></a>
+                                            </div>
+                                            <div class="portfolio-content">
+                                                <h4><a href="portfolio-single.html">Creative Marketing</a></h4>
+                                                <p>Animation, Branding</p>
+                                            </div>
+                                        </div>
+                                        <!--/ End Single Portfolio -->
+                                    </div>
+                                    <div class="cbp-item branding consulting">
+                                        <!-- Single Portfolio -->
+                                        <div class="single-portfolio">
+                                            <div class="portfolio-head overlay">
+                                                <img src="https://via.placeholder.com/600x415" alt="#">
+                                                <a class="more" href="portfolio-single.html"><i
+                                                        class="fa fa-long-arrow-right"></i></a>
+                                            </div>
+                                            <div class="portfolio-content">
+                                                <h4><a href="portfolio-single.html">Creative Marketing</a></h4>
+                                                <p>Branding, Consulting</p>
+                                            </div>
+                                        </div>
+                                        <!--/ End Single Portfolio -->
+                                    </div>
+                                    <div class="cbp-item business marketing">
+                                        <!-- Single Portfolio -->
+                                        <div class="single-portfolio">
+                                            <div class="portfolio-head overlay">
+                                                <img src="https://via.placeholder.com/600x415" alt="#">
+                                                <a class="more" href="portfolio-single.html"><i
+                                                        class="fa fa-long-arrow-right"></i></a>
+                                            </div>
+                                            <div class="portfolio-content">
+                                                <h4><a href="portfolio-single.html">Creative Marketing</a></h4>
+                                                <p>Business</p>
+                                            </div>
+                                        </div>
+                                        <!--/ End Single Portfolio -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--/ End Services -->
+@endsection
