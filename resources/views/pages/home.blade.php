f070ce40a40f1547633ea7fafbf279cc4c929ccf
hisarcode <hisarcode@gmail.com>
2021-07-13 09:58:47 +0700

    add home view page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,277 @@
+@extends('layouts.landing')
+
+@section('content')
+    <!-- Hero Slider -->
+    <section class="hero-slider style1">
+        <div class="home-slider">
+            <!-- Single Slider -->
+            <div class="single-slider" style="background-image:url('https://via.placeholder.com/1700x800.png')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-8 col-12">
+                            <div class="welcome-text">
+                                <div class="hero-text">
+
+                                    <h1>Inovasi-Inovasi Daerah Kabupaten Sanggau</h1>
+                                    <div class="p-text">
+                                        <p>Bagian ini merupakan bagian konten utama yang akan memuat
+                                            carousel-carousel dari gambar inovasi yang ingin ditampilkan</p>
+                                    </div>
+                                    <div class="button">
+                                        <a href="contact.html" class="bizwheel-btn theme-1 effect">Lihat Lainnya</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->
+            <!-- Single Slider -->
+            <div class="single-slider" style="background-image:url('https://via.placeholder.com/1700x800.png')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-8 col-12">
+                            <div class="welcome-text">
+                                <div class="hero-text">
+                                    <h1>Inovasi-Inovasi Daerah Kabupaten Sanggau</h1>
+                                    <div class="p-text">
+                                        <p>Bagian ini merupakan bagian konten utama yang akan memuat
+                                            carousel-carousel dari gambar inovasi yang ingin ditampilkan</p>
+                                    </div>
+                                    <div class="button">
+                                        <a href="blog.html" class="bizwheel-btn theme-1 effect">Lihat Lainnya</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->
+            <!-- Single Slider -->
+            <div class="single-slider" style="background-image:url('https://via.placeholder.com/1700x800.png')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-8 col-12">
+                            <div class="welcome-text">
+                                <div class="hero-text">
+                                    <h1>Inovasi-Inovasi Daerah Kabupaten Sanggau</h1>
+                                    <div class="p-text">
+                                        <p>Bagian ini merupakan bagian konten utama yang akan memuat
+                                            carousel-carousel dari gambar inovasi yang ingin ditampilkan</p>
+                                    </div>
+                                    <div class="button">
+                                        <a href="team.html" class="bizwheel-btn theme-2 effect">Lihat Lainnya</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->
+        </div>
+    </section>
+    <!--/ End Hero Slider -->
+
+    <!-- Counterup -->
+    <section class="counterup">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <!-- Single Counterup -->
+                    <div class="single-counter">
+                        <div class="icon"><i class="fa fa-book"></i></div>
+                        <div class="conter-content">
+                            <div class="counter-head">
+                                <h3><b class="number">1005</b><span>+</span></h3>
+                            </div>
+                            <p>Inovasi Masuk</p>
+                        </div>
+                    </div>
+                    <!--/ End Single Counterup -->
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <!-- Single Counterup -->
+                    <div class="single-counter">
+                        <div class="icon"><i class="fa fa-users"></i></div>
+                        <div class="conter-content">
+                            <div class="counter-head">
+                                <h3><b class="number">400</b><span>+</span></h3>
+                            </div>
+                            <p>Inovasi Terealisasi</p>
+                        </div>
+                    </div>
+                    <!--/ End Single Counterup -->
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <!-- Single Counterup -->
+                    <div class="single-counter">
+                        <div class="icon"><i class="fa fa-life-ring"></i></div>
+                        <div class="conter-content">
+                            <div class="counter-head">
+                                <h3><b class="number">400</b><span>+</span></h3>
+                            </div>
+                            <p>Inovasi Masuk</p>
+                        </div>
+                    </div>
+                    <!--/ End Single Counterup -->
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--/ End Counterup -->
+
+    <!-- Video Feature -->
+    <section class="video-feature side overlay section-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <div class="img-feature ">
+                        <img src="https://via.placeholder.com/800x685.png" alt="Video Thumbnail">
+                        <div class="video-play">
+                            <a href="https://www.youtube.com/watch?v=RLlPLqrw8Q4"
+                                class="video video-popup mfp-iframe">
+                                <i class="fa fa-play"></i>
+                            </a>
+                            <div class="waves-block">
+                                <div class="waves wave-1"></div>
+                                <div class="waves wave-2"></div>
+                                <div class="waves wave-3"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <div class="features-main ">
+                        <div class="title">
+                            <h2>Video Inovasi Daerah</h2>
+                            <p>Ini merupakan penjelasan dari video jika diperlukan</p>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ End Video Feature -->
+
+    <!-- Portfolio -->
+    <section class="portfolio section-space">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12">
+                    <div class="section-title default text-center">
+                        <div class="section-top">
+                            <h1><b>Infografis</b></h1>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="portfolio-main">
+                        <div id="portfolio-item" class="portfolio-item-active">
+                            <div class="cbp-item">
+                                <!-- Single Portfolio -->
+                                <div class="single-portfolio">
+                                    <div class="portfolio-head overlay">
+                                        <img src="https://via.placeholder.com/600x415" alt="#">
+                                        <a class="more" href="portfolio-single.html"><i
+                                                class="fa fa-long-arrow-right"></i></a>
+                                    </div>
+                                    <div class="portfolio-content">
+                                        <h4><a href="portfolio-single.html">Lorem Ipsum</a></h4>
+                                        <p>Title</p>
+                                    </div>
+                                </div>
+                                <!--/ End Single Portfolio -->
+                            </div>
+                            <div class="cbp-item">
+                                <!-- Single Portfolio -->
+                                <div class="single-portfolio">
+                                    <div class="portfolio-head overlay">
+                                        <img src="https://via.placeholder.com/600x415" alt="#">
+                                        <a class="more" href="portfolio-single.html"><i
+                                                class="fa fa-long-arrow-right"></i></a>
+                                    </div>
+                                    <div class="portfolio-content">
+                                        <h4><a href="portfolio-single.html">Lorem Ipsum</a></h4>
+                                        <p>Title</p>
+                                    </div>
+                                </div>
+                                <!--/ End Single Portfolio -->
+                            </div>
+                            <div class="cbp-item">
+                                <!-- Single Portfolio -->
+                                <div class="single-portfolio">
+                                    <div class="portfolio-head overlay">
+                                        <img src="https://via.placeholder.com/600x415" alt="#">
+                                        <a class="more" href="portfolio-single.html"><i
+                                                class="fa fa-long-arrow-right"></i></a>
+                                    </div>
+                                    <div class="portfolio-content">
+                                        <h4><a href="portfolio-single.html">Lorem Ipsum</a></h4>
+                                        <p>Title</p>
+                                    </div>
+                                </div>
+                                <!--/ End Single Portfolio -->
+                            </div>
+                            <div class="cbp-item">
+                                <!-- Single Portfolio -->
+                                <div class="single-portfolio">
+                                    <div class="portfolio-head overlay">
+                                        <img src="https://via.placeholder.com/600x415" alt="#">
+                                        <a class="more" href="portfolio-single.html"><i
+                                                class="fa fa-long-arrow-right"></i></a>
+                                    </div>
+                                    <div class="portfolio-content">
+                                        <h4><a href="portfolio-single.html">Lorem Ipsum</a></h4>
+                                        <p>Title</p>
+                                    </div>
+                                </div>
+                                <!--/ End Single Portfolio -->
+                            </div>
+                            <div class="cbp-item">
+                                <!-- Single Portfolio -->
+                                <div class="single-portfolio">
+                                    <div class="portfolio-head overlay">
+                                        <img src="https://via.placeholder.com/600x415" alt="#">
+                                        <a class="more" href="portfolio-single.html"><i
+                                                class="fa fa-long-arrow-right"></i></a>
+                                    </div>
+                                    <div class="portfolio-content">
+                                        <h4><a href="portfolio-single.html">Lorem Ipsum</a></h4>
+                                        <p>Title</p>
+                                    </div>
+                                </div>
+                                <!--/ End Single Portfolio -->
+                            </div>
+                            <div class="cbp-item">
+                                <!-- Single Portfolio -->
+                                <div class="single-portfolio">
+                                    <div class="portfolio-head overlay">
+                                        <img src="https://via.placeholder.com/600x415" alt="#">
+                                        <a class="more" href="portfolio-single.html"><i
+                                                class="fa fa-long-arrow-right"></i></a>
+                                    </div>
+                                    <div class="portfolio-content">
+                                        <h4><a href="portfolio-single.html">Lorem Ipsum</a></h4>
+                                        <p>Title</p>
+                                    </div>
+                                </div>
+                                <!--/ End Single Portfolio -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ End Portfolio -->
+@endsection
