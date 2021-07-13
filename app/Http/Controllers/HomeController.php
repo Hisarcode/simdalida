d3df6a90eaf316df0e82d5454951ca4b7e7e5eb7
hisarcode <hisarcode@gmail.com>
2021-07-13 09:56:10 +0700

    add home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('pages.home', [
+            'title_page' => 'Home'
+        ]);
     }
 }
