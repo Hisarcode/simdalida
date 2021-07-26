127e9e0a2fc65e090fe7f00c425ca9b3b21b9f39
hisarcode <hisarcode@gmail.com>
2021-07-26 20:44:36 +0700

    Update notification on dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,10 +8,33 @@ use Illuminate\Http\Request;
 use App\Models\InnovationProposal;
 use App\Models\InnovationReport;
 use App\Models\InnovationProfile;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    /**
+     * Count the period of now.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get_current_triwulan()
+    {
+        // check hari ini masuk periode apa berdasarkan bulan
+        $current_month = \Carbon\Carbon::now('Asia/Jakarta')->month;
+        if ($current_month >= 1 && $current_month <= 3) {
+            $current_triwulan = 1;
+        } elseif ($current_month >= 4 && $current_month <= 6) {
+            $current_triwulan = 2;
+        } elseif ($current_month >= 7 && $current_month <= 9) {
+            $current_triwulan = 3;
+        } elseif ($current_month >= 10 && $current_month <= 12) {
+            $current_triwulan = 4;
+        }
+
+        return $current_triwulan;
+    }
+
     public function index(Request $request)
     {
 
@@ -19,16 +42,56 @@ class DashboardController extends Controller
             $proposal = InnovationProposal::orderBy('id', 'DESC')->count();
             $report = InnovationReport::orderBy('id', 'DESC')->count();
             $is_improvement = Complain::where('is_improvement', 'belum')->count();
+            $current_triwulan = "";
+            $notifications = "";
         } else if (Auth::user()->roles == 'ADMIN') {
             $proposal = InnovationProposal::where('users_id', Auth::user()->id)->count();
             $report = InnovationReport::where('users_id', Auth::user()->id)->count();
             $is_improvement = '';
+            $current_triwulan = $this->get_current_triwulan();
+
+
+            $innovation_profiles = InnovationProfile::with(['innovation_proposal'])->where('users_id', Auth::user()->id)->orderBy('id', 'DESC')->get();
+
+
+            $notification_collection = collect();
+            $a = 0;
+            foreach ($innovation_profiles as $innovation_profile) {
+
+                for ($quartal_iteration = 1; $quartal_iteration <= $current_triwulan; $quartal_iteration++) {
+
+                    $a = $quartal_iteration;
+                    $current_year =  \Carbon\Carbon::now('Asia/Jakarta')->year;
+
+                    $innovation_report = InnovationReport::where('innovation_profiles_id', $innovation_profile->id)->where('report_year', $current_year)->where('quartal', $quartal_iteration)->get();
+
+
+                    $notification = collect();
+
+                    if (blank($innovation_report)) {
+                        $notification->name = $innovation_profile->name;
+                        $notification->quartal = $quartal_iteration;
+                        $notification->message = "Laporan Triwulan Ke-" . $quartal_iteration . " belum dimasukkan";
+                        $notification_collection->push($notification);
+                    }
+
+                    $innovation_report = '';
+                }
+            }
+
+            $notifications = $notification_collection->sortBy([
+                ['name', 'asc'],
+                ['quartal', 'asc'],
+            ])->values()->all();
         }
+
 
         return view('pages.admin.dashboard', [
             'proposal' => $proposal,
             'report' => $report,
             'is_improvement' => $is_improvement,
+            'notifications' => $notifications,
+            'current_triwulan' => $current_triwulan,
         ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -18,7 +18,8 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Proposal Inovasi</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Proposal
+                                Inovasi</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $proposal }}</div>
                         </div>
                         <div class="col-auto">
@@ -35,7 +36,8 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Laporan Inovasi</div>
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Laporan Inovasi
+                            </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $report }}</div>
                         </div>
                         <div class="col-auto">
@@ -46,31 +48,63 @@
             </div>
         </div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            @if (Auth::user()->roles == 'SUPERADMIN')
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-info shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Pengaduan yang Belum Ditindaklanjuti</div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $is_improvement }}</div>
-                                    </div>
+        <!-- Earnings (Monthly) Card Example -->
+        @if (Auth::user()->roles == 'SUPERADMIN')
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Pengaduan yang
+                                Belum Ditindaklanjuti</div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $is_improvement }}</div>
                                 </div>
                             </div>
-                            <div class="col-auto">
-                                <i class="fas fa-spinner fa-2x text-gray-300"></i>
-                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-spinner fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
-            </div>   
-            @endif
-        
+            </div>
+        </div>
+        @endif
+
     </div>
 
+    @if (Auth::user()->roles == 'ADMIN')
+    <div class="row">
+        <div class="col-xl-6 mb-4">
+            <div class="card shadow">
+                <div class="card-header bg-primary py-3">
+                    <h6 class="m-0 font-weight-bold text-gray-100">Notifikasi</h6>
+                </div>
+                <div class="card-body ">
+                    <div class="overflow-auto" style="height:300px;
+                    overflow-y: scroll;">
+
+
+                        @forelse ($notifications as $notification)
+                        <div class="card mb-4 py-0 my-0 border-bottom-warning">
+                            <div class="card-body p-1 m-1">
+                                <h6 class="font-weight-bold">{{ $notification->name }}</h6>
+                                <p>{{ $notification->message  }}</p>
+                            </div>
+                        </div>
+                        @empty
+                        <p class="text-center">Tidak ada notifikasi</p>
+                        <hr>
+                        @endforelse
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endif
 
 </div>
 <!-- /.container-fluid -->
