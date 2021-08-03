7b037afc4fb80c6a1f094cc19e194e2b32a3b0cf
hisarcode <hisarcode@gmail.com>
2021-08-03 16:14:11 +0700

    add active class to tab active

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -10,58 +10,60 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
+    <li class="nav-item {{ request()->is('admin') ?'active' : '' }}">
         <a class="nav-link" href="/admin">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
 
     @if (Auth::user()->roles == 'SUPERADMIN')
-    <li class="nav-item">
+    <li class="nav-item {{ request()->is('admin/user') ?'active' : '' }}">
         <a class="nav-link" href="{{ route('user.index') }}">
             <i class="fas fa-fw fa-hotel"></i>
             <span>Manajemen User/Admin</span></a>
     </li>
     @endif
 
-    <li class="nav-item">
+    <li class="nav-item {{ request()->is('admin/innovation-proposal') ?'active' : '' }}">
         <a class="nav-link" href="{{ route('innovation-proposal.index') }}">
             <i class="fas fa-fw fa-hotel"></i>
             <span>Proposal Inovasi</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ request()->is('admin/innovation-profile') ?'active' : '' }}">
         <a class="nav-link" href="{{ route('innovation-profile.index') }}">
             <i class="fas fa-fw fa-home"></i>
             <span>Profil Inovasi</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ request()->is('admin/innovation-report') ?'active' : '' }}">
         <a class="nav-link" href="{{ route('innovation-report.index') }}">
             <i class="fas fa-fw fa-sign"></i>
             <span>Laporan Inovasi</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ request()->is('admin/complain-inbox') ?'active' : '' }}">
         <a class="nav-link" href="{{ route('complain-inbox.index') }}">
             <i class="fas fa-fw fa-envelope-open-text"></i>
             <span>Daftar Pengaduan</span></a>
     </li>
 
     @if (Auth::user()->roles == 'SUPERADMIN')
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Konten Website</span>
+    <li
+        class="nav-item {{ request()->is('admin/carousel') || request()->is('admin/infographic-content') || request()->is('admin/about') ?'active' : '' }}">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Konten Website</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <a class="collapse-item" href="{{ route('carousel.index') }}">Carousel</a>
-            <a class="collapse-item" href="{{ route('about.index') }}">Tentang Kami</a>
-            <a class="collapse-item" href="{{ route('infographic-content.index') }}">Infografis</a>
-          </div>
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('carousel.index') }}">Carousel</a>
+                <a class="collapse-item" href="{{ route('about.index') }}">Tentang Kami</a>
+                <a class="collapse-item" href="{{ route('infographic-content.index') }}">Infografis</a>
+            </div>
         </div>
-      </li>
+    </li>
     @endif
 
 
