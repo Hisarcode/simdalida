4a4b5b0a5a5f04492e66738de7884daee4091f59
hisarcode <hisarcode@gmail.com>
2021-08-21 13:16:44 +0700

    update infographic

--- app/Http/Controllers/InfographicController.php
+++ app/Http/Controllers/InfographicController.php
@@ -6,17 +6,57 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\InnovationProfile;
 use App\Models\About;
+use App\Models\InnovationReport;
 
 class InfographicController extends Controller
 {
+    /**
+     * Count the period of now.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get_current_triwulan()
+    {
+        // check hari ini masuk periode apa berdasarkan bulan
+        $current_month = \Carbon\Carbon::now('Asia/Jakarta')->month;
+        if ($current_month >= 1 && $current_month <= 3) {
+            $current_triwulan = 1;
+        } elseif ($current_month >= 4 && $current_month <= 6) {
+            $current_triwulan = 2;
+        } elseif ($current_month >= 7 && $current_month <= 9) {
+            $current_triwulan = 3;
+        } elseif ($current_month >= 10 && $current_month <= 12) {
+            $current_triwulan = 4;
+        }
+
+        return $current_triwulan;
+    }
+
     public function index()
     {
-        $infographic = InnovationProfile::with(['innovation_proposal'])->orderBy('users_id', 'ASC')->get();
+        $infographics = InnovationProfile::with(['innovation_proposal'])->orderBy('users_id', 'ASC')->get();
         $about = About::orderBy('id', 'DESC')->get();
+        $current_year =  \Carbon\Carbon::now('Asia/Jakarta')->year;
+        $current_triwulan = $this->get_current_triwulan();
+
+
+        foreach ($infographics as $infographic) {
+
+            $innovation_report = InnovationReport::where('innovation_proposals_id', $infographic->innovation_proposals_id)
+                ->where('report_year', $current_year)->count();
+
+            // dd($current_triwulan);
+
+            if ($innovation_report < $current_triwulan) {
+                $infographic->sign = 0;
+            } else {
+                $infographic->sign = 1;
+            }
+        }
 
         return view('pages.infographic', [
             'title_page' => 'Infografis',
-            'infographic' => $infographic,
+            'infographic' => $infographics,
             'about' => $about
         ]);
     }
@@ -29,9 +69,8 @@ class InfographicController extends Controller
 
         $about = About::orderBy('id', 'DESC')->get();
 
-        $title_page = InnovationProfile::where('id', $id)->first()->name;
-
-        $post = InnovationProfile::with(['innovation_proposal'])->get()->random(4);
+        $title_page = InnovationProfile::where('id', $id)->first()->innovation_proposal->name;
+        $post = InnovationProfile::with(['innovation_proposal'])->get()->take(1);
 
         return view('pages.infographicdetail', compact('infographic', 'title_page', 'post', 'about'));
     }
--- resources/views/pages/infographic.blade.php
+++ resources/views/pages/infographic.blade.php
@@ -1,78 +1,83 @@
 @extends('layouts.landing')
 
-@section('content')    
-    <!-- Blog Single -->
-    <section class="news-area archive blog-single section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg col-12">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="blog-single-main">
-                                <div class="main-image">
-                                    <h2 class="sidebar-title blog-title pb-4">Inovasi-Inovasi Daerah</h2>
-                                </div>
-
+@section('content')
+<!-- Blog Single -->
+<section class="news-area archive blog-single section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg col-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="blog-single-main">
+                            <div class="main-image">
+                                <h2 class="sidebar-title blog-title pb-4">Inovasi-Inovasi Daerah</h2>
                             </div>
+
                         </div>
                     </div>
+                </div>
 
 
-                    <div class="row mt-5">
-                        <div class="col-12">
-                            <div class="portfolio-main">
-                              
+                <div class="row mt-5">
+                    <div class="col-12">
+                        <div class="portfolio-main">
 
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                            <thead>
-                                                <tr>
-                                                    <th>No</th>
-                                                    <th>Perangkat Daerah</th>
-                                                    <th>Nama Inovasi Daerah</th>
-                                                    <th>Bentuk Inovasi</th>
-                                                    <th>Tipe inovasi</th>
-                                                    <th>Lihat Detail</th>
-                                                </tr>
-                                            </thead>
-                        
-                                            <tbody>
-                                                <?php
+
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Perangkat Daerah</th>
+                                            <th>Nama Inovasi Daerah</th>
+                                            <th>Bentuk Inovasi</th>
+                                            <th>Tipe inovasi</th>
+                                            <th>Lihat Detail</th>
+                                            <th>*</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php
                                                 $i = 1;
                                                 ?>
-                                                @forelse ($infographic as $profile)
-                                                <tr>
-                                                    <td>{{ $i++ }}</td>
-                                                    <td>{{ $profile->user->name }}</td>
-                                                    <td>{{ $profile->name }}</td>
-                                                    <td>{{ $profile->innovation_proposal->innovation_formats }}</td>
-                                                    <td>{{ $profile->innovation_proposal->innovation_type }}</td>
-                                                    <td>
-                                                        <a href="{{ route('infographic-detail', $profile->id) }}" class="btn btn-info">
-                                                            <i class="fa fa-eye"></i>
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                                @empty
-                                                <tr>
-                                                    <td colspan="6" class="text-center">
-                                                        Data Kosong
-                                                    </td>
-                                                </tr>
-                                                @endforelse
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                
-                              
+                                        @forelse ($infographic as $profile)
+                                        <tr>
+                                            <td>{{ $i++ }}</td>
+                                            <td>{{ $profile->user->name }}</td>
+                                            <td>{{ $profile->name }}</td>
+                                            <td>{{ $profile->innovation_proposal->innovation_formats }}</td>
+                                            <td>{{ $profile->innovation_proposal->innovation_type }}</td>
+                                            <td>
+                                                <a href="{{ route('infographic-detail', $profile->id) }}"
+                                                    class="btn btn-info">
+                                                    <i class="fa fa-eye"></i>
+                                                </a>
+                                            </td>
+                                            <td>@if ($profile->sign == 0)
+                                                <span class="text-danger">*</span>
+                                                @endif</td>
+                                        </tr>
+                                        @empty
+                                        <tr>
+                                            <td colspan="6" class="text-center">
+                                                Data Kosong
+                                            </td>
+                                        </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
                             </div>
+
+
                         </div>
                     </div>
-
                 </div>
 
             </div>
+
         </div>
-    </section>
-    <!--/ End Services -->
+    </div>
+</section>
+<!--/ End Services -->
 @endsection
